db9e59f9a55c711c279fafa31ff0b2e404a2633c
freemind <me@mathpaquette.com>
2022-02-06 14:20:14 -0800

    build: rename to build-iq

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'distributed-task-queue-root',
+  selector: 'build-iq-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- apps/frontend/src/app/nx-welcome.component.ts
+++ apps/frontend/src/app/nx-welcome.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 /* eslint-disable */
 
 @Component({
-  selector: 'distributed-task-queue-nx-welcome',
+  selector: 'build-iq-nx-welcome',
   template: `
     <!--
      * * * * * * * * * * * * * * * * * * * * * * * * * * * *
