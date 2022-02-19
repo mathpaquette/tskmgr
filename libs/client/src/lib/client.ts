17409c518d736f73ff16c7dacac30b6e0b7319ff
Math Paquette <me@mathpaquette.com>
2022-02-19 02:46:42 -0800

    feat: create common node library

--- libs/client/src/lib/client.ts
+++ libs/client/src/lib/client.ts
@@ -1,3 +1,71 @@
 export function client(): string {
   return 'client';
 }
+
+import { spawn } from 'child_process';
+
+export interface ProducerOptions {
+  pullRequestId: string;
+  buildId: string;
+}
+
+export interface ConsumerOptions {
+}
+
+export class Client {
+  //constructor(private readonly options: ClientOptions) {}
+
+  // public static create(options: ClientOptions): Client {
+  //   return new Client(options);
+  // }
+
+  public createTasks(): Promise<> {
+
+  }
+
+  public executeTasks(): Promise<void> {
+    const clients = [];
+
+    for (let i = 0; i < this.options.size; i++) {
+      spawn();
+    }
+
+    return Promise.all();
+  }
+
+
+}
+
+
+
+async function defaultTaskRunner(): Promise<void> {
+  let _continue = true;
+
+  while (_continue) {
+
+
+
+  }
+}
+
+
+// function executor(): Promise<void> {
+//
+//   return new Promise((resolve, reject) => {
+//
+//
+//
+//   })
+//
+
+
+
+
+
+
+
+
+}
+
+
+
