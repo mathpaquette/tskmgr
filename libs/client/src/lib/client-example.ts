b65fc4f5e4efff1293ff97957ed495df821cf61e
Math Paquette <me@mathpaquette.com>
2022-02-27 08:02:15 -0800

    refactor(client): update client example

---
+++ libs/client/src/lib/client-example.ts
@@ -0,0 +1,56 @@
+/**
+ * IntelliJ debug:
+ *  Node parameters: --require ts-node/register --require tsconfig-paths/register
+ *  Environment variables: TS_NODE_PROJECT=libs/client/tsconfig.lib.json
+ */
+
+import { execSync } from 'child_process';
+import { CreateTaskDto, Task } from '@tskmgr/common';
+import { ClientFactory } from './client-factory';
+import { v4 as uuid } from 'uuid';
+
+delete process.env.TS_NODE_PROJECT;
+
+const client = ClientFactory.createNew('http://localhost:3333', '1', 1, dataCallback, errorCallback);
+
+(async () => {
+  try {
+    const printAffected = execSync('nx print-affected --all');
+    const printAffectedO = JSON.parse(printAffected.toString());
+
+    const tasks: CreateTaskDto[] = [];
+
+    printAffectedO.projects
+      .filter((x) => !x.includes('-e2e'))
+      .forEach((project) => {
+        tasks.push({ name: project, type: 'lint', command: `nx lint api --skip-nx-cache`, options: { shell: true } });
+        tasks.push({ name: project, type: 'test', command: `nx test api --skip-nx-cache`, options: { shell: true } });
+        tasks.push({ name: project, type: 'build', command: `nx build api --skip-nx-cache`, options: { shell: true } });
+      });
+
+    const newBuild = await client.createBuild({ name: uuid(), type: '123', pullRequestId: '123' });
+    console.log(newBuild);
+
+    const createdTasks = await client.createTasks(newBuild._id, { tasks });
+    console.log(createdTasks);
+
+    const closeBuild = await client.closeBuild(newBuild._id);
+    console.log(closeBuild);
+
+    await client.runTasks(newBuild._id);
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+})();
+
+function dataCallback(task: Task, data: any, cached: () => void): void {
+  console.log(`${data}`);
+  if (data.includes(`${task.name}:${task.type}  [existing outputs match the cache, left as is]`)) {
+    cached();
+  }
+}
+
+function errorCallback(task: Task, data: any): void {
+  console.log(`${data}`);
+}
