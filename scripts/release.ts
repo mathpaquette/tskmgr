cb9ebd309447dddc56a625b00c1ba4a762b71ef2
Math Paquette <me@mathpaquette.com>
2023-01-19 18:50:22 -0500

    ci: docker (wip)

---
+++ scripts/release.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// @ts-ignore
+import yargs from 'yargs/yargs';
+
+const argv = yargs(process.argv.slice(2)).options({
+  a: { type: 'boolean', default: false },
+  b: { type: 'string', demandOption: true },
+  c: { type: 'number', alias: 'chill' },
+  d: { type: 'array' },
+  e: { type: 'count' },
+  f: { choices: ['1', '2', '3'] }
+}).argv;
