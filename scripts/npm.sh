9bcd3a220aa46b261ec97f4e797fe05d6105321c
Math Paquette <me@mathpaquette.com>
2023-01-25 21:14:39 -0500

    migrations 5

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ -z "${TSKMGR_VERSION}" ]; then
   echo "TSKMGR_VERSION variable not set!"
   exit 1
 fi
 
+echo "Bumping @tskmgr/common version"
+cd libs/common && npm version "${TSKMGR_VERSION}" && cd -
+echo "Bumping @tskmgr/client version"
+cd libs/client && npm version "${TSKMGR_VERSION}" && cd -
+
 rm -rf dist
-npx nx reset
-
-cd libs/common && npm version ${TSKMGR_VERSION} && cd -
-cd libs/client && npm version ${TSKMGR_VERSION} && cd -
-
-npx nx build client --verbose
+npx nx build client --verbose --skip-nx-cache
 
 # publish
-cd dist/libs/common && npm publish && cd -
-cd dist/libs/client && npm publish && cd -
+#cd dist/libs/common && npm publish && cd -
+#cd dist/libs/client && npm publish && cd -
 
 # revert
 cd libs/common && git checkout package.json && cd -
--- scripts/release.ts
+++ scripts/release.ts
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
+/**
+ * Usage: npx ts-node scripts/release.ts
+ */
+
 
 // @ts-ignore
 import yargs from 'yargs/yargs';
+import { execSync } from 'child_process';
 
 const argv = yargs(process.argv.slice(2)).options({
   a: { type: 'boolean', default: false },
-  b: { type: 'string', demandOption: true },
-  c: { type: 'number', alias: 'chill' },
-  d: { type: 'array' },
-  e: { type: 'count' },
-  f: { choices: ['1', '2', '3'] }
+  // newVersion: { type: 'string', demandOption: true },
+  // c: { type: 'number', alias: 'chill' },
+  // d: { type: 'array' },
+  // e: { type: 'count' },
+  // f: { choices: ['1', '2', '3'] },
 }).argv;
+
+execSync(`sh scripts/npm.sh`, { stdio: 'inherit' });
