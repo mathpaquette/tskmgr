8ad7c56d1947d995c8faf808a9540bc2c40e8124
Mathieu Paquette <me@mathpaquette.com>
2023-05-07 13:38:31 -0400

    feat: add header logo
    closes #77

--- apps/frontend-e2e/src/integration/app.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('frontend', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should have app name displayed', () => {
-    cy.get('.navbar-brand').should('contain', 'tskmgr');
-  });
-});
