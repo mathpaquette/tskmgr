edc73f777d1bd937b5fa36eabed6a9e0a9ee964c
mathpaquette <me@mathpaquette.com>
2024-03-10 17:51:32 -0400

    chore: update docker.sh script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
+DOCKERHUB_USER=mathpaquette
+TSKMGR_VERSION=$(node -p "require('./package.json').version")
+
+echo "Building docker images for tskmgr v${TSKMGR_VERSION}"
+sleep 3
+
 docker compose -f docker-compose.build.yml build
-docker compose -f docker-compose.yml up
 
-# docker tag mathpaquette/tskmgr-api:$TSKMGR_VERSION mathpaquette/tskmgr-api:latest
-# docker tag mathpaquette/tskmgr-frontend:$TSKMGR_VERSION mathpaquette/tskmgr-frontend:latest
+docker tag ${DOCKERHUB_USER}/tskmgr-migrations:$TSKMGR_VERSION ${DOCKERHUB_USER}/tskmgr-migrations:latest
+docker tag ${DOCKERHUB_USER}/tskmgr-api:$TSKMGR_VERSION ${DOCKERHUB_USER}/tskmgr-api:latest
+docker tag ${DOCKERHUB_USER}/tskmgr-frontend:$TSKMGR_VERSION ${DOCKERHUB_USER}/tskmgr-frontend:latest
 
-# docker push mathpaquette/tskmgr-api --all-tags
-# docker push mathpaquette/tskmgr-frontend --all-tags
+docker push ${DOCKERHUB_USER}/tskmgr-migrations:$TSKMGR_VERSION
+docker push ${DOCKERHUB_USER}/tskmgr-migrations:latest
 
+docker push ${DOCKERHUB_USER}/tskmgr-api:$TSKMGR_VERSION
+docker push ${DOCKERHUB_USER}/tskmgr-api:latest
+
+docker push ${DOCKERHUB_USER}/tskmgr-frontend:$TSKMGR_VERSION
+docker push ${DOCKERHUB_USER}/tskmgr-frontend:latest
+
+# docker compose -f docker-compose.yml up
 # docker-compose up
 # docker run -p 3333:3333 mathpaquette/tskmgr-api
 # docker run -p 8080:8080 mathpaquette/tskmgr-frontend
