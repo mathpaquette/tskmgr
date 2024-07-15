1144e142146d2f3fb51c737cbe24a00c283be155
mathpaquette <me@mathpaquette.com>
2024-07-15 13:47:25 -0400

    chore: use nx release tool

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -4,11 +4,9 @@ set -e
 
 TSKMGR_VERSION=$(node -p "require('./package.json').version")
 
-echo "Bumping @tskmgr/common version"
-cd libs/common && npm version "${TSKMGR_VERSION}" && cd -
-echo "Bumping @tskmgr/client version"
-cd libs/client && npm version "${TSKMGR_VERSION}" && cd -
+npx nx release version "$TSKMGR_VERSION"
 
+# build
 rm -rf dist
 npx nx build client --verbose --skip-nx-cache
 
@@ -17,5 +15,4 @@ cd dist/libs/common && npm publish && cd -
 cd dist/libs/client && npm publish && cd -
 
 # revert
-cd libs/common && git checkout package.json && cd -
-cd libs/client && git checkout package.json && cd -
+npx nx release version "0.0.0"
