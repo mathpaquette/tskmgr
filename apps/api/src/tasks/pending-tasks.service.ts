20cbccf678088501bc5ac31faa9ac75ed8f0198a
smolovyk.oleg <smolovyk.oleg@gmail.com>
2024-10-10 19:08:56 +0000

    fix(api): pending task service fails on managing 800+ tasks #129

--- apps/api/src/tasks/pending-tasks.service.ts
+++ apps/api/src/tasks/pending-tasks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { RunnerInfo, StartTaskDto, StartTaskResponseDto, TaskPriority, TaskStatus } from '@tskmgr/common';
 import { TaskEntity } from './task.entity';
-import { DataSource, EntityManager, In, Not } from 'typeorm';
+import { DataSource, EntityManager, In, Not, QueryFailedError } from 'typeorm';
 import { RunEntity } from '../runs/run.entity';
 import { FindOneOptions } from 'typeorm/find-options/FindOneOptions';
 
@@ -13,57 +13,59 @@ export class PendingTasksService {
    * Get and start one pending task
    */
   async startPendingTask(runId: number, startTaskDto: StartTaskDto): Promise<StartTaskResponseDto> {
-    return await this.dataSource.transaction(async (manager) => {
-      const { runnerId, runnerInfo } = startTaskDto;
-      const run = await manager.findOne(RunEntity, {
-        where: { id: runId },
-        lock: { mode: 'pessimistic_read' },
-      });
+    let run: RunEntity;
+    try {
+      return await this.dataSource.transaction(async (manager) => {
+        const { runnerId, runnerInfo } = startTaskDto;
+        run = await manager.findOne(RunEntity, { where: { id: runId } });
 
-      if (!run) {
-        throw new Error(`Run id: ${runId} can't be found.`);
-      }
-
-      for (const priority of run.prioritization) {
-        if (!Object.values(TaskPriority).includes(priority)) {
-          throw new Error('Unknown priority type!');
+        if (!run) {
+          throw new Error(`Run id: ${runId} can't be found.`);
         }
-      }
 
-      if (run.hasEnded()) {
-        return { continue: false, run };
-      }
+        for (const priority of run.prioritization) {
+          if (!Object.values(TaskPriority).includes(priority)) {
+            throw new Error('Unknown priority type!');
+          }
+        }
 
-      const pendingTask = await this.getTask(manager, runId, run.prioritization);
-      const startedTask = await this.startTask(manager, runnerId, runnerInfo, pendingTask);
+        if (run.hasEnded()) {
+          return { continue: false, run };
+        }
 
-      if (!startedTask) {
-        // check if there is any task left to run
-        const options = this.getOptions({
-          runId,
-          priority: undefined,
-          taskStatus: TaskStatus.Pending,
-        });
-        const pendingTaskExists = await manager.findOne(TaskEntity, options);
-        if (pendingTaskExists) {
+        const allTasks = await manager.find(TaskEntity, this.getOptions({ runId }));
+        const allPendingTasks = allTasks.filter((task) => task.status === TaskStatus.Pending);
+        if (allPendingTasks.length === 0) {
+          const waitingForTasks = !run.closed; // until run is closed, we need to wait for new incoming tasks.
+          return { continue: waitingForTasks, run };
+        }
+
+        let pendingTask: TaskEntity;
+        for (const taskPriority of run.prioritization) {
+          const tasks = allTasks.filter((task) => task.priority === taskPriority);
+          const runningPendingTasks = tasks.filter(
+            (task) => task.status === TaskStatus.Running || task.status === TaskStatus.Pending
+          );
+          pendingTask = await this.findTask(runId, manager, runningPendingTasks);
+          if (pendingTask) {
+            break;
+          }
+        }
+
+        const startedTask = await this.startTask(manager, runnerId, runnerInfo, pendingTask);
+
+        if (!startedTask) {
           return { continue: true, run };
         }
-        const waitingForTasks = !run.closed; // until run is closed, we need to wait for new incoming tasks.
-        return { continue: waitingForTasks, run };
-      }
-
-      return { continue: true, run, task: startedTask };
-    });
-  }
-
-  private async getTask(manager: EntityManager, runId: number, prioritization: TaskPriority[]): Promise<TaskEntity> {
-    for (const priority of prioritization) {
-      const task: TaskEntity = await this.findTask({ manager, runId, priority, searchForNext: true });
-      if (task) {
-        return task;
+        return { continue: true, run, task: startedTask };
+      });
+    } catch (error) {
+      if (error instanceof QueryFailedError && error.message.includes('Deadlock detected')) {
+        return { continue: true, run };
+      } else {
+        throw error;
       }
     }
-    return null;
   }
 
   /**
@@ -80,13 +82,12 @@ export class PendingTasksService {
    */
   private getOptions(params: {
     runId: number;
-    priority: TaskPriority;
-    taskStatus: TaskStatus;
+    priority?: TaskPriority;
+    taskStatus?: TaskStatus;
     taskName?: string;
     exclude?: number[];
   }): FindOneOptions<TaskEntity> {
     const { runId, priority, taskStatus, taskName, exclude = [] } = params;
-
     // Set the order based on the priority
     let order;
     switch (priority) {
@@ -108,7 +109,6 @@ export class PendingTasksService {
         run: { id: runId },
         name: taskName,
         status: taskStatus,
-        priority: priority,
         id: Not(In(exclude)),
       },
       order: order,
@@ -119,104 +119,52 @@ export class PendingTasksService {
     };
   }
 
-  // TODO: refactor to fetch all tasks at once
   /**
-   * This function is used to find a task based on the provided options.
+   * This function is used to find next task to execute.
    * It checks for circular dependencies, whether all dependencies of a task are completed,
    * and if not, it finds the next task that can be executed.
    *
-   * @param params - An object containing the parameters for the find operation.
-   * @param params.manager - The EntityManager instance.
-   * @param params.runId - The ID of the run.
-   * @param params.priority - The priority of the task.
-   * @param params.task - Optional. The task entity.
-   * @param params.seenTasks - Optional. A Map of seen tasks to avoid circular dependencies.
-   * @param params.searchForNext - Optional. A boolean to decide whether to search for the next task if the current task is blocked.
-   *
    * @returns The task that can be executed next, or null if no such task is found.
+   * @param runId
+   * @param priority
+   * @param manager
    */
-  private async findTask(params: {
-    manager: EntityManager;
-    runId: number;
-    priority: TaskPriority;
-    task?: TaskEntity;
-    seenTasks?: Map<number, string>;
-    searchForNext?: boolean;
-  }): Promise<TaskEntity> {
-    const { manager, runId, priority, seenTasks = new Map(), searchForNext = false } = params;
-    let { task } = params;
+  private async findTask(runId: number, manager: EntityManager, tasks: TaskEntity[]): Promise<TaskEntity> {
+    const pendingTasks = tasks.filter((task) => task.status === TaskStatus.Pending);
 
-    // Search for task if it is not provided
-    if (!task) {
-      task = await manager.findOne(TaskEntity, this.getOptions({ runId, priority, taskStatus: TaskStatus.Pending }));
+    if (pendingTasks.length === 0) return null;
+
+    for (const pendingTask of pendingTasks) {
+      const nextTask = this.checkDependencies(pendingTask, pendingTasks, tasks);
+      if (nextTask) {
+        return nextTask;
+      }
     }
+    return null;
+  }
 
-    // Stop execution if task is not found
+  private checkDependencies(task: TaskEntity, pendingTasks: TaskEntity[], allTasks: TaskEntity[]): TaskEntity {
     if (!task) {
+      console.error('Task is not defined. At this point task must be defined');
       return null;
     }
 
-    // Check for circular dependencies
-    if (seenTasks.has(task.id)) {
-      throw new Error('Circular dependency detected');
-    }
-    seenTasks.set(task.id, task.name);
-
-    // Return task if it has no dependencies
-    if (!task.dependsOn || task.dependsOn.length === 0) {
+    // Return the task if all its dependencies are completed, or task don't have dependencies
+    const isReadyToProceed = task.dependsOn.every((dependencyName) => !allTasks.find((t) => t.name === dependencyName));
+    if (isReadyToProceed) {
       return task;
     }
 
-    // Return task if all its dependencies are already completed
-    let allDependenciesCompleted = true;
-    for (const dependency of task.dependsOn) {
-      const options = this.getOptions({
-        runId,
-        priority,
-        taskStatus: TaskStatus.Completed,
-        taskName: dependency,
-      });
-      const dependencyTask = await manager.findOne(TaskEntity, options);
-
-      // If a dependency task is not completed, set the flag to false and break the loop
-      if (!dependencyTask) {
-        allDependenciesCompleted = false;
-        break;
+    for (const dependencyName of task.dependsOn) {
+      const dependencyTask = allTasks.find((t) => t.name === dependencyName);
+      if (dependencyTask?.status !== TaskStatus.Pending) {
+        continue;
       }
-    }
-    // If all dependencies are completed, return the task
-    if (allDependenciesCompleted) {
-      return task;
-    }
-
-    // Search for dependency task
-    for (const dependency of task.dependsOn) {
-      const options = this.getOptions({
-        runId,
-        priority,
-        taskStatus: TaskStatus.Pending,
-        taskName: dependency,
-      });
-      const dependencyTask = await manager.findOne(TaskEntity, options);
-
-      // If dependency task is found, search for the task in it
       if (dependencyTask) {
-        return await this.findTask({ manager, runId, priority, task: dependencyTask });
-      }
-    }
-
-    // If no dependency task is found, it means that at least one of them is in Running state and current task is still blocked,
-    // in this case switch to the next pending task.
-    if (searchForNext) {
-      const options = this.getOptions({
-        runId,
-        priority,
-        taskStatus: TaskStatus.Pending,
-        exclude: Array.from(seenTasks.keys()),
-      });
-      const nextTask = await manager.findOne(TaskEntity, options);
-      if (nextTask) {
-        return await this.findTask({ manager, runId, priority, task: nextTask, seenTasks, searchForNext });
+        const baseDependencyTask = this.checkDependencies(dependencyTask, pendingTasks, allTasks);
+        if (baseDependencyTask) {
+          return baseDependencyTask;
+        }
       }
     }
 
