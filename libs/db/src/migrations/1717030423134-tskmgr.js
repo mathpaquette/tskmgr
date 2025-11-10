8795102855b0cd0db65c4fc48cb20d0dee4fbd3b
Math Paquette <me@mathpaquette.com>
2025-11-10 16:57:24 -0500

    chore: make shema migrations

--- libs/db/src/migrations/1717030423134-tskmgr.js
+++ libs/db/src/migrations/1717030423134-tskmgr.js
@@ -1,13 +1,16 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
+const { MigrationInterface, QueryRunner } = require('typeorm');
+const { schema } = require('../lib/data-source').AppDataSource.options;
 
 module.exports = class tskmgr1717030423134 {
-    name = 'tskmgr1717030423134'
+  name = 'tskmgr1717030423134';
 
-    async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "run" DROP COLUMN "affinity"`);
-    }
+  async up(queryRunner) {
+    await queryRunner.query(`SET search_path TO ${schema}`);
+    await queryRunner.query(`ALTER TABLE "run" DROP COLUMN "affinity"`);
+  }
 
-    async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "run" ADD "affinity" boolean NOT NULL DEFAULT false`);
-    }
-}
+  async down(queryRunner) {
+    await queryRunner.query(`SET search_path TO ${schema}`);
+    await queryRunner.query(`ALTER TABLE "run" ADD "affinity" boolean NOT NULL DEFAULT false`);
+  }
+};
--- libs/db/src/migrations/1717111649620-tskmgr.js
+++ libs/db/src/migrations/1717111649620-tskmgr.js
@@ -1,13 +1,16 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
+const { MigrationInterface, QueryRunner } = require('typeorm');
+const { schema } = require('../lib/data-source').AppDataSource.options;
 
 module.exports = class Tskmgr1717111649620 {
-    name = 'Tskmgr1717111649620'
+  name = 'Tskmgr1717111649620';
 
-    async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "task" ADD "dependsOn" text NOT NULL DEFAULT ''`);
-    }
+  async up(queryRunner) {
+    await queryRunner.query(`SET search_path TO ${schema}`);
+    await queryRunner.query(`ALTER TABLE "task" ADD "dependsOn" text NOT NULL DEFAULT ''`);
+  }
 
-    async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "task" DROP COLUMN "dependsOn"`);
-    }
-}
+  async down(queryRunner) {
+    await queryRunner.query(`SET search_path TO ${schema}`);
+    await queryRunner.query(`ALTER TABLE "task" DROP COLUMN "dependsOn"`);
+  }
+};
