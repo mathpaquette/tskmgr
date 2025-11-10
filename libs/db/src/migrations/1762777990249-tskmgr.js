685e7f4045a0c00765a216959dc8272c73041a01
Math Paquette <me@mathpaquette.com>
2025-11-10 07:39:36 -0500

    chore: tweaks on the db

--- libs/db/src/migrations/1762777990249-tskmgr.js
+++ libs/db/src/migrations/1762777990249-tskmgr.js
@@ -4,16 +4,16 @@ module.exports = class Tskmgr1762777990249 {
     name = 'Tskmgr1762777990249'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "task" DROP COLUMN "dependsOn"`);
-        await queryRunner.query(`ALTER TABLE "task" ADD "dependencies" text NOT NULL DEFAULT ''`);
+        await queryRunner.query(`ALTER TABLE "task" RENAME COLUMN "dependsOn" TO "dependencies"`);
         await queryRunner.query(`ALTER TABLE "task" ADD "hash" character varying(44)`);
         await queryRunner.query(`CREATE UNIQUE INDEX "IDX_da2fb68519fa0bcf5a0c6e7ace" ON "task" ("run_id", "name") `);
+        await queryRunner.query(`CREATE INDEX "IDX_task_hash_ended_at_desc" ON "task" ("hash", "ended_at" DESC)`);
     }
 
     async down(queryRunner) {
+        await queryRunner.query(`DROP INDEX "public"."IDX_task_hash_ended_at_desc"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_da2fb68519fa0bcf5a0c6e7ace"`);
         await queryRunner.query(`ALTER TABLE "task" DROP COLUMN "hash"`);
-        await queryRunner.query(`ALTER TABLE "task" DROP COLUMN "dependencies"`);
-        await queryRunner.query(`ALTER TABLE "task" ADD "dependsOn" text NOT NULL DEFAULT ''`);
+        await queryRunner.query(`ALTER TABLE "task" RENAME COLUMN "dependencies" TO "dependsOn"`);
     }
 }
