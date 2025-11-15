92ecb1efd1bf7744e2c5a25cf8468a39c7a07603
Math Paquette <me@mathpaquette.com>
2025-11-14 19:16:37 -0500

    feat(client): add retry logic to fetch
    fixes #145

--- libs/client/src/lib/client.spec.ts
+++ libs/client/src/lib/client.spec.ts
@@ -1,5 +1,36 @@
+import { Client } from './client';
+import { ClientFactory } from './client-factory';
+
 describe('client', () => {
-  it('should work', () => {
-    //
+  let client: Client;
+
+  beforeEach(() => {
+    client = ClientFactory.createNew('http://localhost:3000', 'runner-1');
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('should retry when rejected promise', async () => {
+    // arrange
+    vi.spyOn(global, 'fetch')
+      .mockRejectedValueOnce(new Error())
+      .mockResolvedValue({ ok: true, json: async () => ({}) } as Response);
+    // act
+    await client.uploadRunFile(1, './README.md', {});
+    // assert
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+  });
+
+  it('should retry when fetch response not ok', async () => {
+    // arrange
+    vi.spyOn(global, 'fetch')
+      .mockResolvedValueOnce({ ok: false, statusText: 'Internal Server Error' } as Response)
+      .mockResolvedValue({ ok: true, json: async () => ({}) } as Response);
+    // act
+    await client.uploadRunFile(1, './README.md', {});
+    // assert
+    expect(global.fetch).toHaveBeenCalledTimes(2);
   });
 });
