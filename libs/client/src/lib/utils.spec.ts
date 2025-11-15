c90110bf72b7df847af9eb0508b315c9a71a5795
mathpaquette <me@mathpaquette.com>
2025-11-15 17:04:33 -0500

    test(client): migrate to vitest

--- libs/client/src/lib/utils.spec.ts
+++ libs/client/src/lib/utils.spec.ts
@@ -3,8 +3,8 @@ import { spawnAsync } from './utils';
 describe('Utils', () => {
   it('should call dataHandler', async () => {
     // arrange
-    const dataHandler = jest.fn();
-    const errorHandler = jest.fn();
+    const dataHandler = vi.fn();
+    const errorHandler = vi.fn();
     // act
     const command = `node -e 'console.log("some data"); process.exit(0)'`;
     await expect(spawnAsync(command, [], { shell: true }, { dataHandler, errorHandler })).resolves.toBeDefined();
@@ -15,8 +15,8 @@ describe('Utils', () => {
 
   it('should call errorHandler', async () => {
     // arrange
-    const dataHandler = jest.fn();
-    const errorHandler = jest.fn();
+    const dataHandler = vi.fn();
+    const errorHandler = vi.fn();
     // act
     const command = `node -e 'console.error("some error"); process.exit(1)'`;
     await expect(spawnAsync(command, [], { shell: true }, { dataHandler, errorHandler })).rejects.toBeDefined();
