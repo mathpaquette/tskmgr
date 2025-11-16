b9aaadb60c5c3840fe32ee8a60bf082db3a6108c
mathpaquette <me@mathpaquette.com>
2025-11-16 16:09:48 -0500

    chore: remove cypress

--- apps/frontend-e2e/src/integration/execution.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('execution', () => {
-  it('should uniquely identify duplicated commands', () => {
-    // arrange
-    cy.intercept('/api/runs/587', { fixture: 'execution/details.json' });
-    cy.intercept('/api/runs/587/tasks', { fixture: 'execution/tasks.json' });
-    // act
-    cy.visit('/runs/587/execution');
-  });
-});
